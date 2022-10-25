55dd431e9d6f81093c099f5d75f548d183042956
majuh <majuhlins76@gmail.com>
2022-10-25 19:37:19 -0400

    25/10

--- exercicio5.7/Program.cs
+++ exercicio5.7/Program.cs
@@ -1,16 +1,29 @@
 ﻿//atividade 5.7: algoritmo que calcula a mediana em um intervalo definido pelo usuário.
 
-int numinicial, numfinal = 0;
+int numinicial, numfinal;
+List<int> numeros = new List<int>();
 double mediana;
 
-Console.WriteLine("Insira o número inicial: ");
-numinicial = int.Parse(Console.ReadLine());
-Console.WriteLine("Insira o número final: ");
-numfinal = int.Parse(Console.ReadLine());
+Console.Write("Insira o número inicial: ");
+numinicial = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insira o número final: ");
+numfinal = Convert.ToInt32(Console.ReadLine());
 
 for (int i = numinicial; i <= numfinal; i++)
 {
-
+    numeros.Add(i);
 }
+
+if (numeros.Count % 2 == 0)
+{
+    mediana = (numeros[(numeros.Count / 2) - 2] + numeros[(numeros.Count / 2) - 1] / 2);
+}
+
+else
+{
+    mediana = numeros[(numeros.Count / 2)];
+}
+
+Console.WriteLine("A mediana é: " + mediana);
 
 Console.ReadKey();
---
+++ exericio5.10/Program.cs
@@ -0,0 +1,5 @@
+﻿//atividade 5.10: algoritmo que calcula descontos relativos ao imposto de renda.
+
+
+
+Console.ReadKey();
